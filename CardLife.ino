a4869472ba3be63ac2944503a2e86e86da0b6f94
Pggamer_!2 <harrison.eric.michael@gmail.com>
2025-12-09 11:49:51 -0500

    Add files via upload

--- CardLife.ino
+++ CardLife.ino
@@ -27,9 +27,8 @@ int viewy = 135;
 #define TFT_ORANGE 0xFFA5
 int runt = 0;
 int res = 1;
-
 int rcolor = random(0xFFFF);
-
+bool instantBoot = true;
 
 
 //Number of generations
@@ -38,36 +37,35 @@ int gens = NUMGEN;
 uint16_t genCount = 0;
 
 void setup() {
-
   auto cfg = M5.config();
   M5Cardputer.begin(cfg, true);
   Serial.begin(9600);  // Initialize serial communication for debugging
-  Serial.println("booting");
+  Serial.println("Starting up");
   M5Cardputer.Display.setRotation(2);
   int textsize = M5Cardputer.Display.height() / 60;
   if (textsize == 0) {
     textsize = 1;
   }
   M5Cardputer.Display.setTextSize(textsize);
+  if (instantBoot != true) {
+    M5Cardputer.Display.setCursor(0, 0);
 
-  M5Cardputer.Display.setCursor(0, 0);
+    extern const unsigned char gImage_002[];
 
+    M5Cardputer.Display.fillScreen(WHITE);
+    M5Cardputer.Display.drawBitmap(0, 0, 80, 160, (uint16_t *)gImage_002);
 
-  extern const unsigned char gImage_002[];
+    delay(500);
 
-  M5Cardputer.Display.fillScreen(WHITE);
-  M5Cardputer.Display.drawBitmap(0, 0, 80, 160, (uint16_t *)gImage_002);
+    M5Cardputer.Display.setRotation(1);
+    M5Cardputer.Display.setTextSize(2);
+    M5Cardputer.Display.setTextColor(BLACK, WHITE);
+    M5Cardputer.Display.setCursor(5, 50);
+    M5Cardputer.Display.println(F(" CARD-LIFE "));
 
-  delay(500);
-
-  M5Cardputer.Display.setRotation(1);
-  M5Cardputer.Display.setTextSize(2);
-  M5Cardputer.Display.setTextColor(BLACK, WHITE);
-  M5Cardputer.Display.setCursor(5, 50);
-  M5Cardputer.Display.println(F(" CARD-LIFE "));
-
-  delay(2000);
-  M5Cardputer.Display.fillScreen(WHITE);
+    delay(2000);
+    M5Cardputer.Display.fillScreen(WHITE);
+  }
 }
 //Current grid
 uint8_t grid[GRIDX][GRIDY];
@@ -110,7 +108,7 @@ void loop() {
         initGrid();
         int GEN_DELAY = 20;
       }
-      if (M5Cardputer.Keyboard.isKeyPressed('2')) { //[CALLS SETRES FUNCTION TO CHANGE THE RESOLUTION]
+      if (M5Cardputer.Keyboard.isKeyPressed('2')) {  //[CALLS SETRES FUNCTION TO CHANGE THE RESOLUTION]
         Serial.println("2 Pressed");
         setres();
         M5Cardputer.Display.setRotation(1);
@@ -214,7 +212,7 @@ int getNumberOfNeighbors(int x, int y) {
   return grid[x - 1][y] + grid[x - 1][y - 1] + grid[x][y - 1] + grid[x + 1][y - 1] + grid[x + 1][y] + grid[x + 1][y + 1] + grid[x][y + 1] + grid[x - 1][y + 1];
 }
 
-// void statusSys() { [NO CLUE WHAT THIS FUNCITON DOES BUT NEVER GETS CALLED]
+// void statusSys() { [NO CLUE WHAT THIS FUNCITON DOES (havn't looked at it), BUT NEVER GETS CALLED]
 //   M5Cardputer.update();
 //   M5Cardputer.Display.setRotation(1);
 //   extern const unsigned char gImage_002[];
@@ -228,7 +226,7 @@ int getNumberOfNeighbors(int x, int y) {
 
 //   M5Cardputer.Display.setTextColor(BLACK, WHITE);
 //   if (M5Cardputer.Keyboard.isChange()) {
-//     if (M5Cardputer.Keyboard.isKeyPressed('3')) {
+//     if (M5Cardputer.Keyboard.isKeyPressed()) {
 //       Serial.println("3 Pressed");
 
 
@@ -251,51 +249,59 @@ int getNumberOfNeighbors(int x, int y) {
 
 // menu select resolution [NEEDS TO BE REBUILT TO MAINTIAN FUNCTION LOOP]
 void setres() {
-  extern const unsigned char gImage_002[];
-  M5Cardputer.Display.setRotation(2);
-  rcolor == random(0xFFFF);
-  M5Cardputer.Display.setTextSize(2);
-  M5Cardputer.Display.drawBitmap(0, 0, 80, 160, (uint16_t *)gImage_002);
-  M5Cardputer.Display.setRotation(1);
-  if (M5Cardputer.Keyboard.isChange()) {
-    if (M5Cardputer.Keyboard.isKeyPressed('5')) {
-      Serial.println("5 Pressed");
+  M5Cardputer.Display.clear();
+  bool Active = true;
+  while (Active) {
+    int prevRes = 0;
+    M5Cardputer.update();
+    if (prevRes != res) {  //Update Screen
       M5Cardputer.Display.setCursor(5, 5);
-      M5Cardputer.Display.setTextColor(BLACK, WHITE);
+      M5Cardputer.Display.setTextColor(WHITE, BLACK);
       M5Cardputer.Display.println(F(" CARD-LIFE  "));
       M5Cardputer.Display.setCursor(5, 60);
       M5Cardputer.Display.println(F(" set Res"));
       M5Cardputer.Display.setCursor(120, 60);
       M5Cardputer.Display.println(res);
+      prevRes = res;
+    }
+
+    if (M5Cardputer.Keyboard.isChange()) {
+      if (M5Cardputer.Keyboard.isKeyPressed('`')) {
+        Active = false;
+      }
       if (M5Cardputer.Keyboard.isKeyPressed('4')) {
         Serial.println("4 Pressed");
         res++;
         rcolor == random(0xFFFF);
       }
-      if (res == 5) { res = 1; }
+      if (res == 5) {
+        res = 1;
+      }
       CELLXY == res;
-      if (res == 1) {
-        viewx = 240;
-        viewy = 135;
-        gens = 3999;
-      }
-      if (res == 2) {
-        viewx = 120;
-        viewy = 67;
-        gens = 1999;
-      }
-      if (res == 3) {
-        viewx = 40;
-        viewy = 30;
-        gens = 800;
-      }
-      if (res == 4) {
-        viewx = 30;
-        viewy = 20;
-        gens = 500;
+      switch (res) {
+        case 1:
+          viewx = 240;
+          viewy = 135;
+          gens = 3999;
+          break;
+        case 2:
+          viewx = 120;
+          viewy = 67;
+          gens = 1999;
+          break;
+        case 3:
+          viewx = 40;
+          viewy = 30;
+          gens = 800;
+          break;
+        case 4:
+          viewx = 30;
+          viewy = 20;
+          gens = 500;
+          break;
       }
       delay(50);
     }
   }
-  M5Cardputer.Display.fillScreen(BLACK);
+  M5Cardputer.Display.clear();
 }
